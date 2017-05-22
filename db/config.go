124643dd4bd9e9ec3461901633da347321eeebdd
David McClure <dmcclure@pivotal.io>
2017-05-22 14:20:25 -0700

    MySQL connection string includes parseTime option
    [#144803733]

--- db/config.go
+++ db/config.go
@@ -24,7 +24,7 @@ func (c Config) ConnectionString() (string, error) {
 		ms := (time.Duration(c.Timeout) * time.Second).Nanoseconds() / 1000 / 1000
 		return fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=disable&connect_timeout=%d&read_timeout=%d&write_timeout=%d", c.User, c.Password, c.Host, c.Port, c.DatabaseName, ms, ms, ms), nil
 	case "mysql":
-		return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?timeout=%ds&readTimeout=%ds&writeTimeout=%ds", c.User, c.Password, c.Host, c.Port, c.DatabaseName, c.Timeout, c.Timeout, c.Timeout), nil
+		return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?parseTime=true&timeout=%ds&readTimeout=%ds&writeTimeout=%ds", c.User, c.Password, c.Host, c.Port, c.DatabaseName, c.Timeout, c.Timeout, c.Timeout), nil
 	default:
 		return "", fmt.Errorf("database type '%s' is not supported", c.Type)
 	}
