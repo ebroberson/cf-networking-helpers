8b9dd63803a5273d385b5b2a63c811719618cb75
Joachim Valdez <valdezj@us.ibm.com>
2017-08-03 16:20:44 -0400

    Log Wrapper places logger on request context
    [#149572867]

--- middleware/log_wrapper.go
+++ middleware/log_wrapper.go
@@ -1,22 +1,29 @@
 package middleware
 
 import (
+	"context"
 	"net/http"
 
 	"code.cloudfoundry.org/lager"
 )
 
-type LoggableHandlerFunc func(logger lager.Logger, w http.ResponseWriter, r *http.Request)
+type Key string
 
-func LogWrap(logger lager.Logger, loggableHandlerFunc LoggableHandlerFunc) http.HandlerFunc {
+const LoggerKey = Key("logger")
+
+func LogWrap(logger lager.Logger, wrappedHandler http.HandlerFunc) http.HandlerFunc {
+
 	return func(w http.ResponseWriter, r *http.Request) {
 		requestLogger := logger.Session("request", lager.Data{
 			"method":  r.Method,
 			"request": r.URL.String(),
 		})
+		contextWithLogger := context.WithValue(r.Context(), LoggerKey, requestLogger)
+		r = r.WithContext(contextWithLogger)
+
 		requestLogger.Debug("serving")
 		defer requestLogger.Debug("done")
 
-		loggableHandlerFunc(requestLogger, w, r)
+		wrappedHandler(w, r)
 	}
 }
--- middleware/log_wrapper_test.go
+++ middleware/log_wrapper_test.go
@@ -28,20 +28,25 @@ var HaveLogData = func(nextMatcher types.GomegaMatcher) types.GomegaMatcher {
 		return log.Data
 	}, nextMatcher)
 }
-
-var _ = Describe("LogWrap", func() {
+var _ = Describe("LogConWrap", func() {
 	var (
 		logger              *lagertest.TestLogger
-		loggableHandlerFunc middleware.LoggableHandlerFunc
+		loggableHandlerFunc http.HandlerFunc
 	)
+
 	BeforeEach(func() {
 		logger = lagertest.NewTestLogger("test-session")
 		logger.RegisterSink(lager.NewWriterSink(GinkgoWriter, lager.DEBUG))
-		loggableHandlerFunc = func(logger lager.Logger, w http.ResponseWriter, r *http.Request) {
-			logger = logger.Session("logger-group")
-			logger.Info("written-in-loggable-handler")
+		loggableHandlerFunc = func(w http.ResponseWriter, r *http.Request) {
+			if v := r.Context().Value(middleware.LoggerKey); v != nil {
+				if logger, ok := v.(lager.Logger); ok {
+					logger = logger.Session("logger-group")
+					logger.Info("written-in-loggable-handler")
+				}
+			}
 		}
 	})
+
 	It("creates \"request\" session and passes it to LoggableHandlerFunc", func() {
 		handler := middleware.LogWrap(logger, loggableHandlerFunc)
 		req, err := http.NewRequest("GET", "http://example.com", nil)
