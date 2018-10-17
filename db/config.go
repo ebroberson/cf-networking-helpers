2fe22126a4ffc796d4baff7e005f0d3f27e2b3f4
Christian Ang <cang@pivotal.io>
2018-10-17 09:31:38 -0700

    WIP: Add skip hostname verification to mysql conn
    - Also add client cert and client key
    
    [#160840442]
    
    Signed-off-by: Michael Oleske <moleske@pivotal.io>

--- db/config.go
+++ db/config.go
@@ -3,10 +3,6 @@ package db
 import (
 	"fmt"
 	"time"
-	"io/ioutil"
-	"crypto/x509"
-	"crypto/tls"
-	"github.com/go-sql-driver/mysql"
 )
 
 type Config struct {
@@ -19,6 +15,8 @@ type Config struct {
 	DatabaseName string `json:"database_name" validate:""`
 	RequireSSL   bool   `json:"require_ssl" validate:""`
 	CACert       string `json:"ca_cert" validate:""`
+	ClientCert   string
+	ClientKey    string
 }
 
 func (c Config) ConnectionString() (string, error) {
@@ -30,49 +28,11 @@ func (c Config) ConnectionString() (string, error) {
 		ms := (time.Duration(c.Timeout) * time.Second).Nanoseconds() / 1000 / 1000
 		return fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=disable&connect_timeout=%d", c.User, c.Password, c.Host, c.Port, c.DatabaseName, ms), nil
 	case "mysql":
-		return c.buildMysqlConnectionString()
+		mysqlConnectionStringBuilder := &MySQLConnectionStringBuilder{
+			MySQLAdapter: &MySQLAdapter{},
+		}
+		return mysqlConnectionStringBuilder.Build(c)
 	default:
 		return "", fmt.Errorf("database type '%s' is not supported", c.Type)
 	}
-}
-
-func (c Config) buildMysqlConnectionString() (string, error) {
-	connString := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?parseTime=true", c.User, c.Password, c.Host, c.Port, c.DatabaseName)
-	dbConfig, err := mysql.ParseDSN(connString)
-	if err != nil {
-		return "", fmt.Errorf("parsing db connection string: %s", err)
-	}
-
-	timeoutDuration := time.Duration(c.Timeout) * time.Second
-	dbConfig.Timeout = timeoutDuration
-	dbConfig.ReadTimeout = timeoutDuration
-	dbConfig.WriteTimeout = timeoutDuration
-
-	if c.RequireSSL {
-		certBytes, err := ioutil.ReadFile(c.CACert)
-		if err != nil {
-			return "", fmt.Errorf("reading db ca cert file: %s", err)
-		}
-
-		caCertPool := x509.NewCertPool()
-		if ok := caCertPool.AppendCertsFromPEM(certBytes); !ok {
-			return "", fmt.Errorf("appending cert to pool from pem - invalid cert bytes")
-		}
-
-		tlsConfig := &tls.Config{
-			InsecureSkipVerify: false,
-			RootCAs:            caCertPool,
-		}
-
-		tlsConfigName := fmt.Sprintf("%s-tls", c.DatabaseName)
-
-		err = mysql.RegisterTLSConfig(tlsConfigName, tlsConfig)
-		if err != nil {
-			return "", fmt.Errorf("registering mysql tls config: %s", err)
-		}
-
-		dbConfig.TLSConfig = tlsConfigName
-	}
-
-	return dbConfig.FormatDSN(), nil
 }
